d9267d06d7a44ed35f2f5bc7f51f71114cb0c4f8
Bryan A <bryanacostadelacruz@gmail.com>
2021-11-17 11:41:36 -0600

    quiz question functionality

--- assets/script.js
+++ assets/script.js
@@ -1,84 +1,75 @@
 var content_box = document.querySelector("#container");
-let quizQuestions = [
+var quizQuestions = [
     {
         Question: "What does HTML stand for?",
         correct: "Hyper Text Markup Language",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["How To Milk Legos","None of your business","Hyper Text Maximum Language","Hyper Text Markup Language"]
     },
     {
         Question: "what does CSS stand for?",
         correct: "Cascading Style Sheets",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["Corn Stalk Searching","Cows Sing Songs","Cascading Super Sheet","Cascading Style Sheets"]
     },
     {
         Question: "What does push() do?",
         correct: "adds one or more elements to the end of an array",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["Pulls","adds a defined string into the end of the body","pushes a variable over","adds one or more elements to the end of an array"]
     },
     {
         Question: "How do you style elements on a page?",
         correct: "CSS",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["HTML","JS","C++","CSS"]
     },
     {
         Question: "What is considered falsy?",
         correct: "0",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["1","no","wrong","0"]
     },
     {
         Question: "What will return true?",
         correct: "return(1==1);",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["return(1);","return(True);","return(1=1);","return(1==1);"]
     },
     {
         Question: "How do you grab an element by its class?",
         correct: "document.querySelector('.id')",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["document.grabClass('.cheese');","document.findClass('.zawardo')","with gloves","document.querySelector('.id')"]
     },
     {
         Question: "how do you console log the letter a?",
         correct: "console.log('a')",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["a","print('a');","console.log('the letter a?')","console.log('a')"]
     },
     {
         Question: "What is a proper for loop",
         correct: "for(var i = 0 ; i < a; i++)",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["i(var i = i; i > i: i--)","for(var for = 0; for > i;i++)","for(i goes up until a then add to i)","for(var i = 0 ; i < a; i++)"]
     },
     {
         Question: "What does i do in a for loop?",
         correct: "behaves as an iterator",
-        a2: "b",
-        a3: "c",
-        a4: "d",
+        options: ["relaxes","plays as the point guard","works hard thats what","behaves as an iterator"]
     },
 ];
 var score = 0;
-var time = 120;
+var quesNum = 0;
+var timeleft = 10;
 
 
-console.log(content_box);
+function makeTimer(){ 
+    if(timeleft <= 0){
+        quizEnd();
+        clearInterval(timeleft);
+    }
+    timeleft--;
+    document.getElementById("countdown").textContent = 'Time: ' + timeleft;
+};
 
-function selectStart(content){
+function selectStart(){
     // Create the introductory
+
+    console.log("selectStart");
+
     var startbox = document.createElement("div");
     startbox.className = "justify-content-center text-center mx-5";
     startbox.id = "introduction";
@@ -103,17 +94,21 @@ function selectStart(content){
     startBtn.addEventListener("click", quizStart);
 }
 
-selectStart(content_box);
+selectStart();
 
 function quizStart(){
+
+    setInterval(makeTimer, 1000);
+
+    console.log("quizStart")
     document.querySelector("#introduction").remove();
-    console.log("hello!!!!!!!");
 
     var questionBox = document.createElement("div");
     questionBox.className = "";
     questionBox.id = "questionBox";
 
     var questionText = document.createElement("h3")
+    questionText.id = "question";
     questionText.className = "my-3";
     questionBox.appendChild(questionText);
 
@@ -132,10 +127,50 @@ function quizStart(){
 
         questionList.appendChild(anwserChoice);
     }
-    console.log(questionBox);
     content_box.appendChild(questionBox);
-    
 
+  quizPopulate();
+  document.getElementById("questionBox").addEventListener("click", anwsertaskHandler);
 };
 
-console.log(quizQuestions);
+function quizPopulate(){
+
+    if(quesNum == 10){
+        quizEnd();
+        return false;
+    }
+
+    console.log("quizpopulate")
+    console.log(quesNum);
+
+    document.querySelector("#question").textContent = quizQuestions[quesNum].Question;
+    var RandQues = [0,1,2,3];
+    RandQues = RandQues.sort((a, b) => 0.5 - Math.random());
+    console.log(RandQues);
+
+
+    document.getElementById("0").textContent = quizQuestions[quesNum].options[RandQues[0]];
+    document.getElementById("1").textContent = quizQuestions[quesNum].options[RandQues[1]];
+    document.getElementById("2").textContent = quizQuestions[quesNum].options[RandQues[2]];
+    document.getElementById("3").textContent = quizQuestions[quesNum].options[RandQues[3]];
+
+}
+
+function anwsertaskHandler(event){
+        if(event.target.textContent == quizQuestions[quesNum].correct){
+            console.log(score);
+            score = score + 10;
+            quesNum++;
+            quizPopulate();
+        }
+
+        else if(event.target.textContent != quizQuestions[quesNum].correct && event.target.className == "btn btn-primary font-weight-bold"){
+            console.log("wrong");
+            quesNum++;
+            quizPopulate();
+        }
+}
+
+function quizEnd(){
+    document.getElementById("questionBox").remove();
+}
